4aaa9e51139c7ee5da448997444e5cf196129c31
Роман <istina82new@gmail.com>
2024-11-26 23:50:08 +0300

    first commit

---
+++ module_2_2.py
@@ -0,0 +1,29 @@
+number_first = int(input("Введите певрое число: "))
+number_second = int(input("Введите второе число: "))
+number_third = int(input("Введите третье число: "))
+print(number_first, number_second, number_third)
+if number_first == number_second == number_third:
+    print(3)
+elif number_first == number_second:
+    print(2)
+elif number_first == number_third:
+    print(2)
+elif number_second == number_third:
+    print(2)
+else:
+    print(0)
+
+    number_first = int(input("Введите певрое число: "))
+    number_second = int(input("Введите второе число: "))
+    number_third = int(input("Введите третье число: "))
+    print(number_first, number_second, number_third)
+    if number_first == number_second == number_third:
+        print(3)
+    elif number_first == number_second:
+        print(2)
+    elif number_first == number_third:
+        print(2)
+    elif number_second == number_third:
+        print(2)
+    else:
+        print(0)
